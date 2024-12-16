5ee775a67ee5a7f9c82db70823b28604c2f9ca2a
VuHoangAnh2110 <134081915+VuHoangAnh2110@users.noreply.github.com>
2024-12-16 18:39:39 +0700

    first commit

---
+++ application/assets/index.js
@@ -0,0 +1,48 @@
+// Thông báo toastr =======================================================
+    $(document).ready(function(){
+        $('#button').on('click', function(){
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": true,
+                "positionClass": "toast-top-right",
+                "preventDuplicates": true,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "2000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            };
+
+            toastr["success"]("Đã có thông báo xịn <3", "Test!")
+        });
+    });
+
+    function ThongBao(type, msg, title){
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": true,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "2000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+
+        toastr[type](msg, title);
+    };
+// ========================================================================
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./application/views/**/*.php",
+    "./application/assets/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
