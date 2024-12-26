a90b98964ee15bff773c48a751d5fa89827d2c5f
VuHoangAnh2110 <134081915+VuHoangAnh2110@users.noreply.github.com>
2024-12-26 16:44:22 +0700

    Create VFooter.php

---
+++ application/views/layout/VFooter.php
@@ -0,0 +1,15 @@
+
+<div>
+    <footer class="bg-gray-800 text-white py-4">
+        <div class="container mx-auto text-center">
+            <p>&copy; 2024 - Hệ Thống Gửi SMS</p>
+        </div>
+    </footer>
+</div>
+
+    <script src="{$base_url}application/assets/toastr/jquery.js"></script>
+    <script src="{$base_url}application/assets/toastr/toastr.min.js"></script>
+    <script src="{$base_url}application/assets/index.js"></script>
+
+</body>
+</html>
